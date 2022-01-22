fe789d3bc2a3d4eb746c0040e4a2b008817da54c
MallepoguAlankritha <mallepogualankritha@gmail.com>
2022-01-22 16:04:51 +0530

    Added :signup and signin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-// import Signin from './components/signin/signin'
-import SignUp from './components/signup/signup';
+import Signin from './components/signin/signin'
+// import SignUp from './components/signup/signup';
 import './App.css';
 
 function App() {
   return (
     <div>
        <Signin />
-       {/* <SignUp /> */} ///this is my love
+       {/* <SignUp /> */} 
     </div>
   );
 }
